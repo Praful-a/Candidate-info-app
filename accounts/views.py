55fafa9d573d740b4746257568d0b06544fe3c5d
Praful-a <prafuldubey27@gmail.com>
2020-08-10 16:31:32 +0530

    added the register user functionality.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserAdminCreationForm
 
 # Create your views here.
+def register(request, *args, **kwargs):
+    form = UserAdminCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('account:login')
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
