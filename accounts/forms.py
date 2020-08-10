5b546f0c3a38074bad90d3c5602621facb7aa3e1
Praful-a <prafuldubey27@gmail.com>
2020-08-10 17:06:53 +0530

    added login and logout functionality.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import User
@@ -48,3 +49,21 @@ class UserAdminChangeForm(forms.ModelForm):
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
         return self.initial["password"]
+
+class UserLoginForm(forms.Form):
+    query = forms.CharField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        query = self.cleaned_data.get('query')
+        password = self.cleaned_data.get('password')
+        user_qs_final = User.objects.filter(
+                Q(email__iexact=query)
+            ).distinct()
+        if not user_qs_final.exists() and user_qs_final.count != 1:
+            raise forms.ValidationError("Invalid credentials - user does not exist")
+        user_obj = user_qs_final.first()
+        if not user_obj.check_password(password):
+            raise forms.ValidationError("credentials are not correct")
+        self.cleaned_data["user_obj"] = user_obj
+        return super(UserLoginForm, self).clean(*args, **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import UserAdminCreationForm
+from django.contrib.auth import login, logout
+from .forms import UserAdminCreationForm, UserLoginForm
 
 # Create your views here.
 def register(request, *args, **kwargs):
@@ -11,3 +12,15 @@ def register(request, *args, **kwargs):
         'form': form
     }
     return render(request, 'accounts/register.html', context)
+
+def login_view(request, *args, **kwargs):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.cleaned_data.get('user_obj')
+        login(request, user_obj)
+        return redirect('candidate:home')
+    return render(request, "accounts/login.html", {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('account:login')
